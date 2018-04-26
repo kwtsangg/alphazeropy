4c1a126691fcc673a77684c3a16fe9461f2ea1de
Ka Wa Tsang <kwtsang@nikhef.nl>
2018-04-26 18:08:47 +0200

    allow to use thinking time instead of n rollout in training

--- train.py
+++ train.py
@@ -90,12 +90,14 @@ class train_pipeline:
     self.temp                  = args.temp
     self.temp_trans_after      = args.temp_trans_after
     self.n_rollout             = args.n_rollout
+    self.s_thinking            = args.s_thinking
+    self,use_thinking          = args.use_thinking
     self.c_puct                = args.c_puct
     self.batch_size            = args.batch_size
     self.epochs                = args.epochs
 
     from mcts_id import MCTS_player # or from mcts_cyclic_ref import MCTS_player
-    self.AI_player = MCTS_player(self.AI_brain.predict, c_puct = self.c_puct, n_rollout = self.n_rollout, is_self_play = True, temp = self.temp)
+    self.AI_player = MCTS_player(self.AI_brain.predict, c_puct = self.c_puct, n_rollout = self.n_rollout, is_self_play = True, temp = self.temp, s_thinking=self.s_thinking, use_thinking=self.use_thinking)
 
     # other training params
     self.play_batch_size             = args.play_batch_size
@@ -289,6 +291,8 @@ if __name__ == "__main__":
   parser.add_argument("--temp",                default=1.,          action="store",            type=float, help="temperature to control how greedy of selecting next action")
   parser.add_argument("--temp-trans-after",    default=20,          action="store",            type=int,   help="after this number of moves, the temperature becomes 0.")
   parser.add_argument("--n-rollout",           default=400,         action="store",            type=int,   help="number of simulations for each move")
+  parser.add_argument("--s-thinking",          default=1,           action="store",            type=int,   help="time allowed to think for each move (in seconds)")
+  parser.add_argument("--use-thinking",        default=False,       action="store_true",                   help="use thinking time instead of n_rollouts")
   parser.add_argument("--c-puct",              default=5.,          action="store",            type=float, help="coefficient of controlling the extent of exploration versus exploitation")
   parser.add_argument("--batch-size",          default=512,         action="store",            type=int,   help="mini-batch size for training")
   parser.add_argument("--epochs",              default=50,          action="store",            type=int,   help="number of training steps for each gradient descent update")
