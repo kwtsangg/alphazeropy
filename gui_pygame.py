a5e4b5971379dc3a58541334c859a619c94bee19
Ka Wa Tsang <kwtsang@nikhef.nl>
2019-02-27 11:30:37 +0100

    make a reset_params functions

--- gui_pygame.py
+++ gui_pygame.py
@@ -4,28 +4,33 @@ import pygame
 from pygame.locals import *
 from gui import *
 
+# Basic parameters
 FPS = 30
 XGAP = [0,0]
 YGAP = [100,0]
-SCREENX = 512 + XGAP[0] + XGAP[1]
-SCREENY = 512 + YGAP[0] + YGAP[1]
-FRAMEX = SCREENX - XGAP[0] - XGAP[1]
-FRAMEY = SCREENY - YGAP[0] - YGAP[1]
-SW = [FRAMEX*0.1 + XGAP[0], FRAMEX*0.9 + XGAP[0]]
-SH = [FRAMEY*0.1 + YGAP[0], FRAMEY*0.9 + YGAP[0]]
-Color_screen = 	(129, 216, 208) # Tiffany
+Color_screen = (129, 216, 208) # Tiffany
 Color_line = (0,0,0)
 
+def reset_params(new_screenx, new_screeny):
+  global SCREENX, SCREENY, FRAMEX, FRAMEY, SW, SH
+  SCREENX = new_screenx
+  SCREENY = new_screeny
+  FRAMEX = SCREENX - XGAP[0] - XGAP[1]
+  FRAMEY = SCREENY - YGAP[0] - YGAP[1]
+  SW = [FRAMEX*0.1 + XGAP[0], FRAMEX*0.9 + XGAP[0]]
+  SH = [FRAMEY*0.1 + YGAP[0], FRAMEY*0.9 + YGAP[0]]
+reset_params(512+XGAP[0]+XGAP[1], 512+YGAP[0]+YGAP[1])
+
 def main():
-  global SCREEN, FPSCLOCK, SCREENX, SCREENY, SW, SH
+  global SCREEN, FPSCLOCK, SCREENX, SCREENY, FRAMEX, FRAMEY, SW, SH
   pygame.init()
   FPSCLOCK = pygame.time.Clock()
   # First time setup
   SCREEN = pygame.display.set_mode((SCREENX, SCREENY),HWSURFACE|DOUBLEBUF|RESIZABLE)
   SCREEN.fill(Color_screen)
-  pygame.display.set_caption('AlphaZero Platform')
+  pygame.display.set_caption('My AlphaZeroPy Platform')
   boardstyle = "grid"
-  draw_board(7,6,boardstyle)
+  draw_board(7,7,boardstyle)
   while True:
     for event in pygame.event.get():
       if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
@@ -34,13 +39,8 @@ def main():
       elif event.type==VIDEORESIZE:
         SCREEN = pygame.display.set_mode(event.dict['size'],HWSURFACE|DOUBLEBUF|RESIZABLE)
         SCREEN.fill(Color_screen)
-        SCREENX = event.dict['size'][0]
-        SCREENY = event.dict['size'][1]
-        FRAMEX = SCREENX - XGAP[0] - XGAP[1]
-        FRAMEY = SCREENY - YGAP[0] - YGAP[1]
-        SW = [FRAMEX*0.1 + XGAP[0], FRAMEX*0.9 + XGAP[0]]
-        SH = [FRAMEY*0.1 + YGAP[0], FRAMEY*0.9 + YGAP[0]]
-        draw_board(7,6,boardstyle)
+        reset_params(event.dict['size'][0], event.dict['size'][1])
+        draw_board(7,7,boardstyle)
     pygame.display.update()
     FPSCLOCK.tick(FPS)
 
