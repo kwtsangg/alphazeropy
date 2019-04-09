e49e0d2333b778c8c9c5291736840585500217aa
Ka Wa Tsang <kwtsang@nikhef.nl>
2019-04-09 21:24:13 +0200

    1) the opponent for evaluation is saved to ensure the opponent is always the same 2) --p1-brain and --p2-brain allow to parse "latest" or "strongest" to load the corresponding AI

--- play.py
+++ play.py
@@ -21,7 +21,7 @@ from datetime import datetime
 from server import Server, Human
 
 #================================================================
-# Main
+# Functions
 #================================================================
 def inverse_logistic(b, P, c_elo=1./400.):
   """
@@ -29,6 +29,58 @@ def inverse_logistic(b, P, c_elo=1./400.):
   """
   return int(b-np.log(1./P-1.)/c_elo)
 
+def find_model(evaluate_model_path, with_elo=True, with_latest=True):
+  # with_latest\with_elo  |     True        |    False
+  #                 True  | Latest_with_elo | Latest_without_elo
+  #                 False |   strongest     |     ---
+  current_model_elo = -1.
+  current_model_no  = 0
+  current_model_dir = ""
+  try: # Python2
+    model_dir_list = os.walk(evaluate_model_path).next()[1]
+  except: # Python3
+    model_dir_list = next(os.walk(evaluate_model_path))[1]
+  for model_dir in model_dir_list:
+    if with_latest:
+      try:
+        model_no = int(model_dir.split("_")[0])
+        if model_no > current_model_no:
+          if with_elo:
+            try:
+              model_elo = np.loadtxt("%s/%s/elo.txt" % (evaluate_model_path, model_dir))
+              if model_elo > current_model_elo:
+                current_model_elo = model_elo
+              else:
+                continue
+            except:
+              continue
+          current_model_no  = int(model_dir.split("_")[0])
+          current_model_dir = model_dir
+      except:
+        pass
+    else:
+      try:
+        model_elo = np.loadtxt("%s/%s/elo.txt" % (evaluate_model_path, model_dir))
+        if model_elo > current_model_elo:
+          current_model_elo = model_elo
+          current_model_no  = int(model_dir.split("_")[0])
+          current_model_dir = model_dir
+      except:
+        pass
+
+  if current_model_no == 0:
+    if with_latest:
+      if with_elo:
+        raise ValueError("Cannot find the latest possible player with elo")
+      else:
+        raise ValueError("Cannot find the latest possible player without elo")
+    else:
+      raise ValueError("Cannot find the strongest player with elo")
+  return current_model_elo, current_model_no, "%s/%s" % (evaluate_model_path, current_model_dir)
+
+#================================================================
+# Main
+#================================================================
 class platform:
   def __init__(self, args):
     # evaluate params
@@ -55,10 +107,6 @@ class platform:
     self.p1_c_puct       = args.p1_c_puct
 
     self.p1_brain_path  = args.p1_brain
-    if self.evaluate and not self.p1_brain_path:
-      print("Loading latest model without elo under %s ..." % self.evaluate_model_path)
-      p1_elo, p1_model_no, self.p1_brain_path = self.load_latest_model(with_elo = False)
-      print("Model %s is loaded into p1 brain as the challenger" % p1_model_no)
     self.p1_name        = args.p1_name
 
     if self.p1_brain_path is None:
@@ -86,10 +134,6 @@ class platform:
     self.p2_c_puct       = args.p2_c_puct
 
     self.p2_brain_path  = args.p2_brain
-    if self.evaluate and not self.p2_brain_path:
-      print("Loading strongest model with elo under %s ..." % self.evaluate_model_path)
-      p2_elo, p2_model_no, self.p2_brain_path = self.load_latest_model(with_elo = True)
-      print("Model %s with elo %i is loaded into p2 brain as the opponent" % (p2_model_no, p2_elo))
     self.p2_name        = args.p2_name
 
     if self.p2_brain_path is None:
@@ -128,43 +172,6 @@ class platform:
     # Other
     self.analysis = args.analysis
 
-  # Functions
-  def load_latest_model(self, with_elo=True):
-    latest_model_elo = -1.
-    latest_model_no  = 0
-    latest_model_dir = ""
-    try: # Python2
-      model_dir_list = os.walk(self.evaluate_model_path).next()[1]
-    except: # Python3
-      model_dir_list = next(os.walk(self.evaluate_model_path))[1]
-    for model_dir in model_dir_list:
-      if with_elo:
-        try:
-          model_elo = np.loadtxt("%s/%s/elo.txt" % (self.evaluate_model_path, model_dir))
-          if model_elo > latest_model_elo:
-            latest_model_elo = model_elo
-            latest_model_no  = int(model_dir.split("_")[0])
-            latest_model_dir = model_dir
-        except:
-          pass
-      else:
-        try:
-          model_elo = np.loadtxt("%s/%s/elo.txt" % (self.evaluate_model_path, model_dir))
-        except:
-          try:
-            model_no = int(model_dir.split("_")[0])
-            if model_no > latest_model_no:
-              latest_model_no  = model_no
-              latest_model_dir = model_dir
-          except:
-            pass
-    if latest_model_no == 0:
-      if with_elo:
-        raise ValueError("The script cannot find a player with elo")
-      else:
-        raise ValueError("The script cannot find a player without elo")
-    return latest_model_elo, latest_model_no, "%s/%s" % (self.evaluate_model_path, latest_model_dir)
-
   # Main modes
   def start_game(self):
     self.server.start_game(player1=self.p1, player2=self.p2, is_gui=True, is_analysis=self.analysis)
@@ -174,12 +181,25 @@ class platform:
     # model_score[0] : [win, lose, draw] as player 1
     # model_score[1] : [win, lose, draw] as player 2
     try:
+      print("Loading the past score ...")
       model_score = np.loadtxt("%s/evaluate.txt" % self.p1_brain_path).T
     except:
       model_score = np.zeros(6).reshape(2,3)
 
-    half_total_game = int(self.evaluate_game // 2)
+    # Save the opponent
+    np.savetxt("%s/evaluate_opponent.txt" % self.p1_brain_path, [self.p2_brain_path], fmt="%s")
 
+    # print summary
+    def print_summary():
+      print("\nSummary: %i/%i" % (np.sum(model_score), self.evaluate_game))
+      print("      play as p1, play as p2")
+      print("win   %11i %11i" % (model_score[0][0], model_score[1][0]))
+      print("lose  %11i %11i" % (model_score[0][1], model_score[1][1]))
+      print("draw  %11i %11i" % (model_score[0][2], model_score[1][2]))
+      print("total %11i %11i" % (sum(model_score[0]), sum(model_score[1])))
+    print_summary()
+
+    half_total_game = int(self.evaluate_game // 2)
     # play as player 1
     while np.sum(model_score[0]) < half_total_game:
       print("\nGenerating gameplay %i/%i ..." % (np.sum(model_score), self.evaluate_game))
@@ -192,13 +212,7 @@ class platform:
         model_score[0][1] += 1.
 
       np.savetxt("%s/evaluate.txt" % self.p1_brain_path, model_score.T, header="play as p1, play as p2")
-
-      print("\nSummary: %i/%i" % (np.sum(model_score), self.evaluate_game))
-      print("      play as p1, play as p2")
-      print("win   %11i %11i" % (model_score[0][0], model_score[1][0]))
-      print("lose  %11i %11i" % (model_score[0][1], model_score[1][1]))
-      print("draw  %11i %11i" % (model_score[0][2], model_score[1][2]))
-      print("total %11i %11i" % (sum(model_score[0]), sum(model_score[1])))
+      print_summary()
 
     # play as player 2
     while np.sum(model_score[1]) < half_total_game:
@@ -212,13 +226,7 @@ class platform:
         model_score[1][1] += 1.
 
       np.savetxt("%s/evaluate.txt" % self.p1_brain_path, model_score.T, header="play as p1, play as p2")
-
-      print("\nSummary: %i/%i" % (np.sum(model_score), self.evaluate_game))
-      print("      play as p1, play as p2")
-      print("win   %11i %11i" % (model_score[0][0], model_score[1][0]))
-      print("lose  %11i %11i" % (model_score[0][1], model_score[1][1]))
-      print("draw  %11i %11i" % (model_score[0][2], model_score[1][2]))
-      print("total %11i %11i" % (sum(model_score[0]), sum(model_score[1])))
+      print_summary()
 
     prob = (np.sum(model_score.T[0]) + 0.5*model_score.T[2])/float(self.evaluate_game)
     if prob == 1.:
@@ -272,23 +280,52 @@ if __name__ == "__main__":
   args = parser.parse_args()
   args.evaluate_model_path = args.evaluate_model_path.format(args.game)
 
+  # If evaluate
   if args.evaluate:
     if args.p1_brain:
+      # Check whether it has been evaluated
       try:
         np.loadtxt("%s/elo.txt" % args.p1_brain)
-        raise ValueError("The model (--p1-brain) has been evaluated")
+        raise ValueError("The model (--p1-brain %s) has been evaluated" % args.p1_brain)
       except:
         pass
     else:
-      print("The script will try looking for the latest possible player without elo in %s as the challenger" % args.evaluate_model_path)
+      print("Looking for the latest possible player without elo in %s as the challenger" % args.evaluate_model_path)
+      p1_elo, p1_model_no, args.p1_brain = find_model(args.evaluate_model_path, with_elo = False, with_latest = True)
+      print("Model %s is loaded into p1 brain as the challenger" % p1_model_no)
 
-    if args.p2_brain:
-      try:
-        np.loadtxt("%s/elo.txt" % args.p2_brain)
-      except:
-        raise ValueError("the model (--p2-brain) has not been evaluated, so it cannot be used to evaluate other model.")
-    else:
-      print("The script will try looking for the strongest possible player with elo in %s as the opponent" % args.evaluate_model_path)
+
+    # Check whether it has an opponent
+    try:
+      args.p2_brain = str(np.loadtxt("%s/evaluate_opponent.txt" % args.p1_brain, dtype=str))
+      print("A past opponent (%s) is found. Overwritting the p2 brain ..." % args.p2_brain)
+    except:
+      if args.p2_brain:
+        try:
+          np.loadtxt("%s/elo.txt" % args.p2_brain)
+        except:
+          raise ValueError("the model (--p2-brain) has not been evaluated, so it cannot be used to evaluate other model.")
+      else:
+        print("Looking for the strongest possible player with elo in %s as the opponent" % args.evaluate_model_path)
+        p2_elo, p2_model_no, args.p2_brain = find_model(args.evaluate_model_path, with_elo = True, with_latest = False)
+        print("Model %s with elo %i is loaded into p2 brain as the opponent" % (p2_model_no, p2_elo))
+  else:
+    if args.p1_brain == "latest":
+      print("Looking for the latest possible player in %s" % args.evaluate_model_path)
+      p1_elo, p1_model_no, args.p1_brain = find_model(args.evaluate_model_path, with_elo = False, with_latest = True)
+      print("Model %s is loaded into p1 brain" % p1_model_no)
+    elif args.p1_brain == "strongest":
+      print("Looking for the strongest player in %s" % args.evaluate_model_path)
+      p1_elo, p1_model_no, args.p1_brain = find_model(args.evaluate_model_path, with_elo = True, with_latest = False)
+      print("Model %s is loaded into p1 brain" % p1_model_no)
+    if args.p2_brain == "latest":
+      print("Looking for the latest possible player in %s" % args.evaluate_model_path)
+      p2_elo, p2_model_no, args.p2_brain = find_model(args.evaluate_model_path, with_elo = False, with_latest = True)
+      print("Model %s is loaded into p2 brain" % p2_model_no)
+    elif args.p2_brain == "strongest":
+      print("Looking for the strongest player in %s" % args.evaluate_model_path)
+      p2_elo, p2_model_no, args.p2_brain = find_model(args.evaluate_model_path, with_elo = True, with_latest = False)
+      print("Model %s is loaded into p2 brain" % p2_model_no)
 
   a = platform(args)
   if args.evaluate:
