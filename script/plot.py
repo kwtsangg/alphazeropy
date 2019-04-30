4de1289edf5658b07d426bb25b77df8f24ad0906
Ka Wa Tsang <kwtsang@nikhef.nl>
2019-04-30 16:33:09 +0200

    update script to plot the training progress and update Makefile

--- script/plot.py
+++ script/plot.py
@@ -1,20 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+import os, sys
 from scipy.interpolate import interp1d
 
 elo  = []
 game = []
+nblocks = []
+name = sys.argv[1]
 
-for filename in os.listdir(os.getcwd()+"/reversi_training_model"):
-  filename = os.getcwd() + "/reversi_training_model/" + filename
+for filename in sorted(os.listdir(os.getcwd()+"/%s_training_model" % name)):
+  try:
+    tmp_nblocks = int(filename.split("_")[-3])
+  except:
+    continue
+  filename = os.getcwd() + "/%s_training_model/" % name + filename
   print filename
   try:
     elo.append(np.loadtxt("%s/elo.txt" % filename))
     game.append(np.loadtxt("%s/game.txt" % filename))
+    nblocks.append(tmp_nblocks)
   except:
     pass
-print elo, game
+print "elo, game, nblocks"
+print np.array([elo, game, nblocks]).T
 x = np.linspace(min(game), max(game), 1000)
 y = interp1d(game, elo)(x)
 plt.scatter(game, elo)
@@ -22,5 +30,13 @@ plt.plot(x, y)
 plt.xlabel("game")
 plt.ylabel("elo")
 plt.grid()
+
+# add a line if nblocks change.
+idx_nblocks_changed = np.append([1], np.diff(nblocks))
+for i in range(len(idx_nblocks_changed)):
+  if idx_nblocks_changed[i]:
+    plt.axvline(game[i], label="nblocks %i" % nblocks[i])
+plt.legend()
+plt.tight_layout()
 plt.show()
 
